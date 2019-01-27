67bd85c848fe7dcd2cd352246afb55ab4fe7d4b7
Panini <1258140199@qq.com>
2019-01-27 13:58:02 +0800

    dev@0.1_daily update

--- src/components/shop/rating/component.js
+++ src/components/shop/rating/component.js
@@ -4,13 +4,32 @@ import SellerCache from '@/axios/seller/cache'
 export default Base.extend({
     data() {
       return {
-        
+        ratings: [],
+        seller: {},
+        isActive:'0',
       };
     },
     methods: {
-      
+      getRatings: function(type) {
+        this.isActive = type;
+        this.sellerCache.getRatings({
+          data:type,
+          success:res => {
+            this.ratings = res;
+          }
+        })
+      },
+      getSeller: function() {
+        this.sellerCache.getSeller({
+          success:res => {
+            this.seller = res;
+          }
+        })
+      }
     },
     created: function() {
       this.sellerCache = new SellerCache() 
+      this.getRatings('0');
+      this.getSeller();
     }
   })
